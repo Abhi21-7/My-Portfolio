5bb7b52be009dc39924b9216cbbcf8244256c1db
Abhi21-7 <abhishekagnihotri2005@gmail.com>
2025-05-29 10:40:31 +0530

    Initial Commiot

---
+++ server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import path from 'path';
+import portfolioRoute from './routes/portfolioRoute.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// static files
+app.use(express.static(path.join(__dirname, '/out'))); 
+
+
+app.use('/api/v1/portfolio', portfolioRoute);
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/out/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
